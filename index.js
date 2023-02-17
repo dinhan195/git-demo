ab06e4de6438ac278f4dfbc2ddb482eddf614c87
ĐÌNH AN <115713179+dinhan195@users.noreply.github.com>
2023-02-17 18:49:55 +0700

    create file test

---
+++ index.js
@@ -0,0 +1,7 @@
+function getDescription(text) {
+    
+    console.log(text);
+    return text.substring(0, 10) + "..."; // write something in the BROWSER and see it in the console
+
+}
+getDescription('hello world ưgrhtery')
